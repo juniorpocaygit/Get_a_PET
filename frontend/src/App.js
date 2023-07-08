3a0c746e7d01235f7d747d8a1940d83a41092d37
juniorpocaygit <juniorpocay@gmail.com>
2023-07-08 00:09:44 -0300

    finalizado autenticação de usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,35 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
+/*Components*/
+import Navbar from'./components/layout/Navbar'
+import Footer from'./components/layout/Footer'
+import Container from'./components/layout/Container'
+import Message from './components/layout/Message'
+
+
 /*pages*/
 import Login from'./components/pages/Auth/Login'
 import Register from'./components/pages/Auth/Register'
 import Home from'./components/pages/Home'
 
+/*Context*/
+import{UserProvider} from './context/UserContext'
+
 function App() {
   return (
     <Router>
-     <Routes>
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/" element={<Home/>} />
-     </Routes>
+      <UserProvider>
+        <Navbar/>
+        <Message/>
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/" element={<Home/>} />
+          </Routes>
+        </Container>
+        <Footer/>
+      </UserProvider>
     </Router>
   );
 }
--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,68 @@
+import {useState, useContext} from'react'
+
+import Input from '../../form/Input'
+import {Link} from 'react-router-dom'
+
+import styles from '../../form/Form.module.css'
+
+/*Context*/
+import{Context} from '../../../context/UserContext'
+
 function Register() {
+    const [user, setUser] = useState({})
+    const {register} = useContext(Context)
+
+    function handleChange(e){
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        register(user)     
+    }
+
     return(
-     <section>
-         <h1>Register</h1>
+     <section className={styles.form_container}>
+         <h1>Cadastrar-se</h1>
+         <form onSubmit={handleSubmit}>
+           <Input
+            text="Nome"
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome."
+            handleOnChange={handleChange}
+           /> 
+           <Input
+            text="Telefone"
+            type="text"
+            name="phone"
+            placeholder="Digite o seu telefone."
+            handleOnChange={handleChange}
+           /> 
+           <Input
+            text="E-mail"
+            type="email"
+            name="email"
+            placeholder="Digite o seu email."
+            handleOnChange={handleChange}
+           /> 
+           <Input
+            text="Senha"
+            type="password"
+            name="password"
+            placeholder="Digite a sua senha."
+            handleOnChange={handleChange}
+           /> 
+           <Input
+            text="Confirmação de Senha"
+            type="password"
+            name="confirmpassword"
+            placeholder="Confirme a sua senha."
+            handleOnChange={handleChange}
+           /> 
+        <input type="submit" value="Cadastrar"/>
+         </form>
+         <p>Já tem conta? <Link to='/login'>Clique aqui.</Link></p>
      </section>
     ) 
  }
